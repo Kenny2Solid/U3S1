e3fd4079855cbccf5728be386498ab359337e857
Kendrick Torrance <torraken000@warren.k12.in.us>
2025-03-07 13:03:17 -0500

    2nd

--- Validation.js
+++ Validation.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError"; // Assign the name for this object to "ValidationError"
+    }
+}
+
+function validateUsername(username) {
+    if (username.length < 10) {
+        throw new ValidationError("Username must be at least 10 characters long");
+    }
+    return "Username is Valid";
+}
+
+try {
+    console.log(validateUsername("abc"));
+} catch (error) {
+    console.error(`${error.name}: ${error.message}`);
+} finally {
+    console.log("All done");
+}
